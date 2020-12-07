53e9c218921bc67c109362e99ddddb947c3323e6
Valente <lucasvillanivalente1@hotmail.com>
2020-12-07 10:35:49 -0300

    terceuri

--- src/main/java/com/apresentacao3semana/AcademiaVivere3Semana/Controller/ClientesController.java
+++ src/main/java/com/apresentacao3semana/AcademiaVivere3Semana/Controller/ClientesController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("vivere/")
@@ -50,31 +51,15 @@ public class ClientesController {
             return clientesRepository.findAll();
     }
 
+
     @GetMapping("/clientes/contabil/{id}")
     ClientesDTO getContabilId(
             @PathVariable(value = "id") Integer clientesDTOId) throws ResourceNotFoundException {
         Clientes clientes = clientesRepository.findById(clientesDTOId)
                 .orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado :: " + clientesDTOId));
 
-
         List <Livro_Caixa> livro_caixas = livros_caixaRepository.findAllByCliente(clientes);
-        List <Livro_CaixaDTO> contabil = new ArrayList<>();
-        AtomicReference<Double> saldoAUX = new AtomicReference<>((double) 0);
-        Collections.sort(livro_caixas);
-        livro_caixas.forEach(livro_caixa -> {
-            Livro_CaixaDTO livro_caixaDTO = new Livro_CaixaDTO();
-            livro_caixaDTO.setData_lancamento(livro_caixa.getData_lancamento());
-            livro_caixaDTO.setDescricao(livro_caixa.getDescricao());
-            livro_caixaDTO.setValor(livro_caixa.getValor());
-            livro_caixaDTO.setTipo(livro_caixa.getTipo());
-            if(livro_caixaDTO.getTipo().equals("C".charAt(0))){
-                saldoAUX.updateAndGet(v -> (double) (v + livro_caixaDTO.getValor()));
-            }else if(livro_caixaDTO.getTipo().equals("D".charAt(0))){
-                saldoAUX.updateAndGet(v -> (double) (v - livro_caixaDTO.getValor()));
-            }
-            livro_caixaDTO.setSaldo(saldoAUX.get());
-            contabil.add(livro_caixaDTO);
-        });
+        List<Livro_CaixaDTO> contabil = getContabil(livro_caixas);
 
         ClientesDTO clientesDTO = new ClientesDTO();
         clientesDTO.setId(clientes.getID());
@@ -87,28 +72,12 @@ public class ClientesController {
     }
 
     @GetMapping("/clientes/contabil")
-    List<ClientesDTO> getContabil(){
+    List<ClientesDTO> getListContabil(){
         List<Clientes> clientes = clientesRepository.findAll();
         List<ClientesDTO> clientesDTO = new ArrayList<>();
     clientes.forEach(cliente -> {
         List <Livro_Caixa> livro_caixas = livros_caixaRepository.findAllByCliente(cliente);
-        List <Livro_CaixaDTO> contabil = new ArrayList<>();
-        AtomicReference<Double> saldoAUX = new AtomicReference<>((double) 0);
-        Collections.sort(livro_caixas);
-        livro_caixas.forEach(livro_caixa -> {
-            Livro_CaixaDTO livro_caixaDTO = new Livro_CaixaDTO();
-            livro_caixaDTO.setData_lancamento(livro_caixa.getData_lancamento());
-            livro_caixaDTO.setDescricao(livro_caixa.getDescricao());
-            livro_caixaDTO.setValor(livro_caixa.getValor());
-            livro_caixaDTO.setTipo(livro_caixa.getTipo());
-            if(livro_caixaDTO.getTipo().equals("C".charAt(0))){
-                saldoAUX.updateAndGet(v -> (double) (v + livro_caixaDTO.getValor()));
-            }else if(livro_caixaDTO.getTipo().equals("D".charAt(0))){
-                saldoAUX.updateAndGet(v -> (double) (v - livro_caixaDTO.getValor()));
-            }
-            livro_caixaDTO.setSaldo(saldoAUX.get());
-            contabil.add(livro_caixaDTO);
-        });
+        List<Livro_CaixaDTO> contabil = getContabil(livro_caixas);
 
         ClientesDTO clienteDTO = new ClientesDTO();
         clienteDTO.setId(cliente.getID());
@@ -120,6 +89,40 @@ public class ClientesController {
     });
         return clientesDTO;
     }
+
+//    @GetMapping("/clientes/contabil/search")
+//    Iterable<ClientesDTO> findByQuery(
+//            @RequestParam(value = "DataInicial", required = false) Date DataInicial, @RequestParam(value = "DataFinal", required = false) Date DataFinal){
+//
+//        List<Clientes> clientes = clientesRepository.findAll();
+//        List<ClientesDTO> clientesDTO = new ArrayList<>();
+//        clientes.forEach(cliente -> {
+//                    List<Livro_Caixa> livro_caixas = livros_caixaRepository.findAllByCliente(cliente);
+//                    List<Livro_CaixaDTO> contabil = getContabil(livro_caixas);
+//
+//                    ClientesDTO clienteDTO = new ClientesDTO();
+//                    clienteDTO.setId(cliente.getID());
+//                    clienteDTO.setCpfCnpj(cliente.getCpfCnpj());
+//                    clienteDTO.setNome(cliente.getNome());
+//                    clienteDTO.setTelefone(cliente.getTelefone());
+//                    clienteDTO.setContabil(contabil);
+//                    clientesDTO.add(clienteDTO);
+//                });
+//        List<ClientesDTO> contabilFiltered = new ArrayList<>();
+//        if (DataInicial != null && DataFinal != null)
+//            contabilFiltered = clientesDTO.stream()
+//                    .filter(DTO -> DTO.getContabil().get(0).getData_lancamento().equals(DataInicial))
+//                    .collect(Collectors.toList());
+//
+//            return contabilFiltered;
+//        else if (DataInicial != null)
+//            return usuarioRepository.findByNome(DataInicial);
+//        else if (DataFinal != null)
+//            return usuarioRepository.findByEmail(DataFinal);
+//        else
+//            return usuarioRepository.findAll();
+//
+//    }
 
     @PostMapping("/clientes")
     Clientes createUser(@Valid @RequestBody Clientes clientes) {
@@ -158,6 +161,27 @@ public class ClientesController {
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
         return response;
+    }
+
+    List<Livro_CaixaDTO> getContabil(List<Livro_Caixa> list){
+        List <Livro_CaixaDTO> contabil = new ArrayList<>();
+        AtomicReference<Double> saldoAUX = new AtomicReference<>((double) 0);
+        Collections.sort(list);
+        list.forEach(livro_caixa -> {
+            Livro_CaixaDTO livro_caixaDTO = new Livro_CaixaDTO();
+            livro_caixaDTO.setData_lancamento(livro_caixa.getData_lancamento());
+            livro_caixaDTO.setDescricao(livro_caixa.getDescricao());
+            livro_caixaDTO.setValor(livro_caixa.getValor());
+            livro_caixaDTO.setTipo(livro_caixa.getTipo());
+            if (livro_caixaDTO.getTipo().equals("C".charAt(0))) {
+                saldoAUX.updateAndGet(v -> (double) (v + livro_caixaDTO.getValor()));
+            } else if (livro_caixaDTO.getTipo().equals("D".charAt(0))) {
+                saldoAUX.updateAndGet(v -> (double) (v - livro_caixaDTO.getValor()));
+            }
+            livro_caixaDTO.setSaldo(saldoAUX.get());
+            contabil.add(livro_caixaDTO);
+        });
+        return contabil;
     }
 
 }
